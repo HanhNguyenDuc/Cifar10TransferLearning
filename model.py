b6a81c49f83d8466b8cadda8aec556e1fef14d8a
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-07-19 10:57:34 +0700

    shuffle data

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ from keras.applications.inception_v3 import InceptionV3
 from keras.applications.resnet50 import ResNet50
 from keras.applications.vgg16 import *
 from keras.preprocessing.image import img_to_array, ImageDataGenerator
+from sklearn.utils import shuffle
 
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 IMAGE_SIZE = X_train.shape[1:]
@@ -52,6 +53,10 @@ model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metr
 
 endp = int(len(X_train) * 0.9)
 
+X_train, y_train = shuffle(X_train, y_train)
+
+print(y_train)
+
 X_val = X_train[endp:]
 y_val = y_train[endp:]
 
@@ -69,9 +74,9 @@ print(X_train.shape)
 
 # model.fit(X_train, y_train, epochs = 50, validation_data = (X_val, y_val))
 datagen.fit(X_train)
-model.fit_generator(datagen.flow(X_train, y_train, batch_size = 32), validation_data = (X_val, y_val), epochs = 25, steps_per_epoch = len(X_train) / 32)
+# model.fit_generator(datagen.flow(X_train, y_train, batch_size = 32), validation_data = (X_val, y_val), epochs = 25, steps_per_epoch = len(X_train) / 32)
 
-loss, score = model.evaluate(X_test, y_test)
+# loss, score = model.evaluate(X_test, y_test)
 
 print(loss, score)
 
